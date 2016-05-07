a3383be766073ebcd53bbcfc47f7dd306ff39035
Johannes Mikulasch <mikulasc@in.tum.de>
2016-05-07 21:19:51 +0900

    Speak the message out loud

--- app/src/main/java/com/example/johannes/myapplication2/DisplayMessageActivity.java
+++ app/src/main/java/com/example/johannes/myapplication2/DisplayMessageActivity.java
@@ -1,6 +1,7 @@
 package com.example.johannes.myapplication2;
 
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
@@ -8,12 +9,15 @@ import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
+    TextToSpeech tts;
+    String message;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
 
         TextView textView = new TextView(this);
         textView.setTextSize(40);
@@ -22,5 +26,20 @@ public class DisplayMessageActivity extends AppCompatActivity {
         RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
         layout.addView(textView);
 
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    tts.speak(message, TextToSpeech.QUEUE_ADD, null);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        tts.shutdown();
     }
 }
