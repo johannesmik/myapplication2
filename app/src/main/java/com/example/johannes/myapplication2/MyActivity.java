aa415f00f219b28f02b3383751051cf8c841abc0
Johannes Mikulasch <mikulasc@in.tum.de>
2016-05-08 16:59:09 +0900

    added a list where a history is kept

--- app/src/main/java/com/example/johannes/myapplication2/MyActivity.java
+++ app/src/main/java/com/example/johannes/myapplication2/MyActivity.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class MyActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.johannes.myapplication2.MESSAGE";
 
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        ListView listview = (ListView) findViewById(R.id.message_list);
+        listview.setAdapter(adapter);
     }
 
     public void sendMessage(View view) {
@@ -22,5 +31,7 @@ public class MyActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+
+        adapter.add(message);
     }
 }
